2a478ddb2f3ecad80c210ac9f9fea4b89346e251
DemersonB <demerson.barros@nomos.com.br>
2025-09-22 19:04:36 -0300

    new: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'brand-primary': '#146eb4', // azul principal
+        'brand-accent': '#ffd814', // amarelo destaque
+        'brand-dark': '#232f3e', // preto/azul escuro
+        'brand-light': '#e3e6e6', // cinza claro
+        'brand-background': '#ffffff', // branco
+      },
+    },
+  },
+  plugins: [],
+};
